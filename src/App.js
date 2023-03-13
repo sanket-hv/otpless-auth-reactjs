a45164e256d06e7e94c1672cc20f299a10514d39
sanket-hv <sankethv8509@gmail.com>
2023-03-13 19:49:35 +0530

    otpless authentication

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
+
+
 
 function App() {
+
+  useEffect(() => {
+    // Define the 'otpless' function
+    window.otpless = (otplessUser) => {
+      // Retrieve the user's details after successful login
+      const waName = otplessUser.waName;
+      const waNumber = otplessUser.waNumber;
+
+      console.log(otplessUser, waName, waNumber)
+      // Handle the signup/signin process
+      // ...
+    };
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +33,7 @@ function App() {
         >
           Learn React
         </a>
+        <div id="otpless"></div>
       </header>
     </div>
   );
